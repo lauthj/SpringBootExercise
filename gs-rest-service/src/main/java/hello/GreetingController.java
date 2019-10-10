2334f64280e18bb8ee34a77b5672eb1ac9a33fe0
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-10-10 16:37:57 -0500

    Joe is one cool dude

--- gs-rest-service/src/main/java/hello/GreetingController.java
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/* Joe is one cool dude */
+
 @RestController
 public class GreetingController {
 
