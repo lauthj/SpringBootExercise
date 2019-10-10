0f33599f7fe8ed2ecc4e3e06761613d8419289ce
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-10-10 16:35:52 -0500

    Joe again

--- gs-rest-service/src/main/java/hello/GreetingController.java
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/*What the fuck!*/
-/* Let's try this again */
-/*Testing*/
-/* What up? */
-
 @RestController
 public class GreetingController {
 
