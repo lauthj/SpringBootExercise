3aabb166487beba7e373d374e71e34aa7c9ded52
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-10-10 16:27:43 -0500

    commit

--- gs-rest-service/src/main/java/hello/GreetingController.java
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
+
 /*What the fuck!*/
-=======
+
 /*Testing*/
->>>>>>> refs/heads/master
+
 @RestController
 public class GreetingController {
 
