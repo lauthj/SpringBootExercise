f6dd6eb2760079d4d7dbe47b2f9bc9dff3f64e41
joseph.lauth <joseph.lauth@CPX-5UCKMCA0IMV.dir.svc.accenture.com>
2019-10-11 13:41:48 -0500

    Committing Ahoy!

--- gs-rest-service/src/main/java/hello/GreetingController.java
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -23,11 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
+    private static final String template2 = "Ahoy!, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    
+    @RequestMapping(value = "/ahoy", method = RequestMethod.GET)
+    public Greeting ahoy(@RequestParam(value="name", defaultValue="Sir Joe") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template2, name));
     }
 }
